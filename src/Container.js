c9d6755d712a11901b9d9b801657461448266883
Kosuke Takeda <june.bure@gmail.com>
2016-03-03 20:33:59 +0900

    add Container

---
+++ src/Container.js
@@ -0,0 +1,14 @@
+/**
+ * Created by TakedaKosuke on 2016/03/03.
+ */
+import React,{ Component} from 'react'
+
+
+export default class Container extends Component {
+    constructor(props) {
+        super(props)
+    }
+    render() {
+        return ({ child })
+    }
+}
