8a63f086fbc00be938c694626ee30debb45f25fc
Kosuke Takeda <june.bure@gmail.com>
2016-03-03 19:47:54 +0900

    slider

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import PortalProject from './PortalProject'
+import PortalSlider from './PortalSlider'
 
 export default class App extends Component {
   constructor(props) {
@@ -6,13 +8,37 @@ export default class App extends Component {
     // 状態の定義
     this.state = { message: "Reactのシンプルなコンポーネントです" }
   }
+  componentDidMount() {
+    this.fetchPortalData()
+  }
+
+  async fetchPortalData() {
+    // Create headers
+    let headers = new Headers()
+    headers.append('Authorization', `Basic ${btoa('spring:intern_2016')}`)
+    // Call API
+    let res = await fetch("https://www.wantedlyapp.com/api/intern/portal", { headers })
+    let json = await res.json()
+    console.log(json) // 受け取った内容がコンソールに出力される
+    this.setState({
+      data: json
+    })
+  }
 
   render() {
+    let popularProject
+    if(this.state.data) {
+      popularProject = this.state.data.data.sections[3]
+    }
     // 見た目の定義
     return(
       <div>
-        <h1>Hello, World!</h1>
-        <p>{ this.state.message }</p>
+        {this.state.data ? (
+            <PortalSlider>
+              {popularProject.projects.map((project) => {
+                return <PortalProject project={project} key={project.id} />
+              })}
+            </PortalSlider>):(<p>Now Loading...</p>)}
       </div>
     )
   }
