cb7f11bc0b96e53a0a48a3003f6ee067513702d7
Kosuke Takeda <june.bure@gmail.com>
2016-03-05 12:27:18 +0900

    refactor App.js

--- src/App.js
+++ src/App.js
@@ -44,75 +44,54 @@ export default class App extends Component {
   }
 
   render() {
-    let eyecatch
-    let project_list
-    let interview
-    let popularProject
-
-    if(this.state.data) {
-      eyecatch = this.state.data.sections[0]
-      project_list = eyecatch.eyecatches[0].project_list
-      interview = eyecatch.eyecatches[1].employee_interview
-      popularProject = this.state.data.sections[3]
-    }
     let data = this.state.data
-//    let popularProject = data && data.sections[3]
-    let newgradProject = data && data.sections[4]
-    let internProject = data && data.sections[5]
+    let projectsSections = data && data.sections.slice(3,6)
+    let eyecatch = data && data.sections[0]
 
-    let myDialog = {
+    let myDialogStyle = {
       width: '90%',
       height: '800px',
       marginTop: '-410px',
       marginLeft: '-46%',
     };
 
-
     return(
       <div className={styles.container}>
         {data ? (
           <div>
-            <div className={styles.section}>
-              <PortalSlider title={popularProject.title}>
-                {
-                  popularProject.projects.map((project) => {
-                    return <div onClick={() => this.refs.simpleDialog.show()}><div onClick={this.toggleDetailPanel.bind(this)}><PortalProject project={project} key={project.id} /></div></div>
-                  })
-                }
-              </PortalSlider>
-              <a onClick={() => this.refs.simpleDialog.show()}>全て見る</a>
-            </div>
-            <div className={styles.section}>
-              <PortalSlider title={newgradProject.title}>
-                {
-                  newgradProject.projects.map((project) => {
-                    return <div onClick={() => this.refs.simpleDialog.show()}><div onClick={this.toggleDetailPanel.bind(this)}><PortalProject project={project} key={project.id} /></div></div>
-                  })
-                }
-              </PortalSlider>
-              <a onClick={() => this.refs.simpleDialog.show()}>全て見る</a>
-            </div>
-            <div className={styles.section}>
-              <PortalSlider title={internProject.title}>
-                {
-                  internProject.projects.map((project) => {
-                    return <div onClick={() => this.refs.simpleDialog.show()}><div onClick={this.toggleDetailPanel.bind(this)}><PortalProject project={project} key={project.id} /></div></div>
-                  })
-                }
-              </PortalSlider>
-              <a onClick={() => this.refs.simpleDialog.show()}>全て見る</a>
-            </div>
-
-            <SkyLight hideOnOverlayClicked ref="simpleDialog" dialogStyles={myDialog} title="">
-              <div>
-                {this.state.data ? (
-                  <PortalPanel>
-                    {popularProject.projects.map((project) => {
-                      return <PortalProject project={project} key={project.id} />
-                    })}
-                  </PortalPanel>):(<p>Now Loading...</p>)}
-              </div>
-            </SkyLight>
+            {
+              projectsSections.map((section,index)=> {
+                return(
+                  <div className={styles.section}>
+                    <PortalSlider title={section.title}>
+                      {
+                        section.projects.map((project) => {
+                          return <PortalProject project={project} key={project.id} />
+                        })
+                      }
+                    </PortalSlider>
+                    <a onClick={() => this.refs['projectsDialog' + index].show()}>全て見る</a>
+                  </div>
+                )
+              })
+            }
+            {
+              projectsSections.map((section,index)=> {
+                return(
+                  <SkyLight hideOnOverlayClicked ref={'projectsDialog' + index} dialogStyles={myDialogStyle} title="">
+                    <div>
+                      {data ? (
+                        <PortalPanel>
+                          {section.projects.map((project) => {
+                            return <PortalProject project={project} key={project.id} />
+                          })}
+                        </PortalPanel>
+                      ):(<p>Now Loading...</p>)}
+                    </div>
+                  </SkyLight>
+                )
+              })
+            }
           </div>
         ):(
           <p>Now Loading...</p>
